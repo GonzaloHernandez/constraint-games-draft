1115fed196b3c407213ba1f6607715991e022fc9
Gonzalo Hernandez <gonzalohernandez@hotmail.com>
2023-11-14 10:41:06 +1100

    Implementation of Reification

--- mzn-python/example6.05/solver.py
+++ mzn-python/example6.05/solver.py
@@ -154,6 +154,47 @@ class Expression (Operable):
         [rmin,rmax] = self.exp2.evaluate()
 
         match self.oper :
+
+            case "==" :
+                if lmin==rmin and lmax==rmax :
+                    self.min = self.max = 1
+                elif lmin > rmax or rmin > lmax :
+                    self.min = self.max = 0
+                else :
+                    [self.min, self.max] = [0,1]
+
+            case "<" :
+                if lmax < rmin :
+                    self.min = self.max = 1
+                elif lmin >= rmax :
+                    self.min = self.max = 0
+                else :
+                    [self.min, self.max] = [0,1]
+
+            case ">" :
+                if lmin > rmax :
+                    self.min = self.max = 1
+                elif lmax <= rmin :
+                    self.min = self.max = 0
+                else :
+                    [self.min, self.max] = [0,1]
+
+            case "<=" :
+                if lmax <= rmin :
+                    self.min = self.max = 1
+                elif lmin > rmax :
+                    self.min = self.max = 0
+                else :
+                    [self.min, self.max] = [0,1]
+
+            case ">=" :
+                if lmin >= rmax :
+                    self.min = self.max = 1
+                elif lmax < rmin :
+                    self.min = self.max = 0
+                else :
+                    [self.min, self.max] = [0,1]
+
             case "+" :
                 [self.min, self.max] = [lmin+rmin , lmax+rmax]
             case "-" :
@@ -163,13 +204,6 @@ class Expression (Operable):
                     min(lmin*rmin, lmin*rmax, lmax*rmin, lmax*rmax),
                     max(lmin*rmin, lmin*rmax, lmax*rmin, lmax*rmax)
                 ]
-            case "==" :
-                if lmin==rmin and lmax==rmax :
-                    self.min = self.max = 1
-                elif lmin > rmax or rmin > lmax :
-                    self.min = self.max = 0
-                else :
-                    [self.min, self.max] = [0,1]
 
         return [self.min, self.max]
 
@@ -182,6 +216,28 @@ class Expression (Operable):
             case "==" :
                 self.exp1.project( max(lmin,rmin), min(lmax,rmax) )
                 self.exp2.project( max(lmin,rmin), min(lmax,rmax) )
+            case "<" :
+                if min == 1 :
+                    self.exp1.project( lmin  , rmax-1 )
+                    self.exp2.project( lmin+1, rmax   )
+                if max == 0 :
+                    self.exp1.project( rmin, lmax )
+                    self.exp2.project( rmin, lmax )                    
+            case ">" :
+                if min == 1 :
+                    self.exp1.project( rmin+1, lmax   )
+                    self.exp2.project( rmin  , lmax-1 )
+                if max == 0 :
+                    self.exp1.project( lmin, rmax )
+                    self.exp2.project( lmin, rmax )
+
+            case "<=" :
+                self.exp1.project( lmin, rmax )
+                self.exp2.project( lmin, rmax )
+            case ">=" :
+                self.exp1.project( rmin, lmax )
+                self.exp2.project( rmin, lmax )
+
             case "+" :
                 self.exp1.project( nmin-rmax , nmax-rmin )
                 self.exp2.project( nmin-lmax , nmax-lmin )
@@ -214,27 +270,44 @@ class Expression (Operable):
 class Constraint :
     def __init__(self, exp) -> None:
         self.exp = exp
+        self.reif = IntVar("_",0,1)
 
     def __str__(self) -> str:
         return str(self.exp)
     
     def prune(self) :
-        self.exp.evaluate()
-        self.exp.project(1,1)
+        [min, max] = self.exp.evaluate()
+        self.exp.project(min,max)
+        self.reif.project(min,max)
+
+    def getReif(self,name) :
+        self.reif.name = name
+        return self.reif
 
 #====================================================================
 
-x = IntVar('x', 2,6)
-y = IntVar('y',-2,5)
-z = IntVar('z', 1,3)
+x   = IntVar('x', 1,3)
+y   = IntVar('y', 1,3)
+z   = IntVar('z', 1,3)
+ux  = IntVar('ux', 0,1)
+uy  = IntVar('uy', 0,1)
+uz  = IntVar('uz', 0,1)
+
+gx  = Constraint(x == y*z)
+gy  = Constraint(y == x*z)
+gz  = Constraint(x*y <= z)
+
+rx   = gx.getReif("rx")
+ry   = gy.getReif("ry")
+rz   = gz.getReif("rz")
 
 i1  = SearchInstance(
-        [x,y,z],
+        [x,y,z,ux,uy,uz],
         [
-            Constraint(
-                x == y * z
-            )
-        ])
+            Constraint(ux==rx), Constraint(uy==ry), Constraint(uz==rz),
+            # gx,gy,gz
+        ]
+    )
 
 i2 = copy.deepcopy(i1)
 
