7ff0aa3b82c52ee1050d3ab18139ea13a66a4e1b
Gonzalo Hernandez <gonzalohernandez@hotmail.com>
2023-11-13 12:07:59 +1100

    HC4 working with with ==, + , and -

--- mzn-python/example6.05/solver.py
+++ mzn-python/example6.05/solver.py
@@ -1,53 +1,65 @@
 import os, copy
 os.system("clear")
 
-# -------------------------------------------------
+#====================================================================
 
 def printlist(ls) :
     print("[ ",end="")
     for l in ls : print(l,end=" ")
     print("]")
 
-# -------------------------------------------------
+#====================================================================
 
 class Operable :
     def __add__(self, exp) :
+        if isinstance(exp, int) : exp = IntVar("_",exp,exp)
         return Expression(self,"+",exp)
 
     def __sub__(self, exp) :
+        if isinstance(exp, int) : exp = IntVar("_",exp,exp)
         return Expression(self,"-",exp)
 
     def __mul__(self, exp) :
+        if isinstance(exp, int) : exp = IntVar("_",exp,exp)
         return Expression(self,"*",exp)
 
     def __mul__(self, exp) :
+        if isinstance(exp, int) : exp = IntVar("_",exp,exp)
         return Expression(self,"*",exp)
 
     def __eq__(self, exp) :
-        return Expression(self,"=",exp)
+        if isinstance(exp, int) : exp = IntVar("_",exp,exp)
+        return Expression(self,"==",exp)
 
     def __ne__(self, exp) :
+        if isinstance(exp, int) : exp = IntVar("_",exp,exp)
         return Expression(self,"!=",exp)
 
     def __lt__(self, exp) :
+        if isinstance(exp, int) : exp = IntVar("_",exp,exp)
         return Expression(self,"<",exp)
 
     def __le__(self, exp) :
+        if isinstance(exp, int) : exp = IntVar("_",exp,exp)
         return Expression(self,"<=",exp)
 
     def __gt__(self, exp) :
+        if isinstance(exp, int) : exp = IntVar("_",exp,exp)
         return Expression(self,">",exp)
 
     def __ge__(self, exp) :
+        if isinstance(exp, int) : exp = IntVar("_",exp,exp)
         return Expression(self,">=",exp)
 
     def __and__(self, exp) :
+        if isinstance(exp, int) : exp = IntVar("_",exp,exp)
         return Expression(self,"&",exp)
 
     def __or__(self, exp) :
+        if isinstance(exp, int) : exp = IntVar("_",exp,exp)
         return Expression(self,"|",exp)
 
-# -------------------------------------------------
+#====================================================================
 
 class IntVar (Operable) :
     def __init__(self, name, min, max) -> None:
@@ -55,6 +67,7 @@ class IntVar (Operable) :
         self.min    = min
         self.max    = max
 
+    #--------------------------------------------------------------
     def __str__(self) -> str:
         if self.isFailed() :
             return f"{self.name}()"
@@ -62,91 +75,39 @@ class IntVar (Operable) :
             return f"{self.name}{{{str(self.min)}}}"
         else :
             return f"{self.name}{{{str(self.min)}..{str(self.max)}}}"
-    
+
+    #--------------------------------------------------------------
     def setge(self, val) :
-        self.min = val
+        self.min = max(self.min, val)
 
     def setle(self, val) :
-        self.max = val
+        self.max = min(self.max, val)
 
     def isAssigned(self) :
         return (self.min==self.max)
     
     def isFailed(self) :
         return (self.min>self.max)
-
-# -------------------------------------------------
-
-class Expression (Operable):
-    def __init__(self, exp1, oper, exp2) -> None:
-        self.exp1 = exp1
-        self.oper = oper
-        self.exp2 = exp2
-
-    def __str__(self) -> str:
-        if self.oper is None :
-            return str(self.exp1)
-        else :
-            return "("+str(self.exp1) + self.oper + str(self.exp2)+")"
-
-    def evaluate(self) :
-        if  isinstance(self.exp1, int) :
-            self.range = [self.exp1,self.exp1]
-
-        if  isinstance(self.exp1, IntVar) :
-            self.range = [self.exp1.min,self.exp1.max]
-        
-        [lmin,lmax] = self.exp1.evaluate()
-        [rmin,rmax] = self.exp2.evaluate()
-
-        match self.oper :
-            case "+" :
-                self.range = [lmin+rmin , lmax+rmax]
-            case "-" :
-                self.range = [lmin-rmax , lmax-rmin]
-            case "*" :
-                self.range = [lmin*rmin , lmax*rmax]
-            case "==" :
-                self.range = [0 , 1]
-
-        return self.range
-
-    def project(self, range) :
-        [newmin,newmax] = range
-
-        if  isinstance(self.exp1, int) :
-            pass
-
-        if  isinstance(self.exp1, IntVar) :
-            self.exp1.setge(newmin)
-            self.exp1.setle(newmax)
-            return
-
-        [prewin,premax] = self.range
-
-        match self.oper :
-            case "+" :
-                pass
-            case "-" :
-                pass
-            case "*" :
-                pass
-            case "==" :
-                pass
-
-        pass
     
-# -------------------------------------------------
+    def evaluate(self) :
+        return [self.min, self.max]
+
+    def project(self, newmin, newmax) :
+        self.setge(newmin)
+        self.setle(newmax)
+
+#====================================================================
 
 class SearchInstance :
     def __init__(self, vars, cons) -> None:
         self.vars = vars
         self.cons = cons
     
+    #--------------------------------------------------------------
     def propagate(self) :
         for c in self.cons :
             c.prune()
-
+        
         for v in self.vars :
             if v.isFailed() :
                 return None
@@ -172,7 +133,64 @@ class SearchInstance :
                     right   .propagate()
                     break
 
-# -------------------------------------------------
+#====================================================================
+
+class Expression (Operable):
+    def __init__(self, exp1, oper, exp2) -> None:
+        self.exp1 = exp1
+        self.oper = oper
+        self.exp2 = exp2
+
+    def __str__(self) -> str:
+        if self.oper is None :
+            return str(self.exp1)
+        else :
+            return "("+str(self.exp1) + self.oper + str(self.exp2)+")"
+
+    #--------------------------------------------------------------
+    def evaluate(self) :
+        [lmin,lmax] = self.exp1.evaluate()
+        [rmin,rmax] = self.exp2.evaluate()
+
+        match self.oper :
+            case "+" :
+                [self.min, self.max] = [lmin+rmin , lmax+rmax]
+            case "-" :
+                [self.min, self.max] = [lmin-rmax , lmax-rmin]
+            case "*" :
+                [self.min, self.max] = [lmin*rmin , lmax*rmax]
+            case "==" :
+                [self.min, self.max] = [max(lmin,rmin), min(lmax,rmax)]
+
+        return [self.min, self.max]
+
+    #--------------------------------------------------------------
+    def project(self, newmin, newmax) :
+        [premin, premax] = [self.min, self.max]
+        [curmin, curmax] = max(premin,newmin), min(premax, newmax)
+
+        match self.oper :
+            case "==" :
+                self.exp1.project(curmin, curmax)
+                self.exp2.project(curmin, curmax)
+            case "+" :
+                [rmin,rmax] = [self.exp2.min, self.exp2.max]
+                [lmin,lmax] = [curmin-rmax,curmax-rmin]
+                self.exp1.project(lmin, lmax)
+
+                [lmin,lmax] = [self.exp1.min, self.exp1.max]
+                [rmin,rmax] = [curmin-lmax,curmax-lmin]
+                self.exp2.project(rmin, rmax)
+            case "-" :
+                [rmin,rmax] = [self.exp2.min, self.exp2.max]
+                [lmin,lmax] = [curmin+rmin,curmax+rmax]
+                self.exp1.project(lmin, lmax)
+
+                [lmin,lmax] = [self.exp1.min, self.exp1.max]
+                [rmin,rmax] = [curmin+lmin,curmax+rmax]
+                self.exp2.project(rmin, rmax)
+
+#====================================================================
 
 class Constraint :
     def __init__(self, exp) -> None:
@@ -183,30 +201,18 @@ class Constraint :
     
     def prune(self) :
         [min,max] = self.exp.evaluate()
-        self.exp.project( [min,max] )
+        self.exp.project( min,max )
 
-# -------------------------------------------------
+#====================================================================
 
-x = IntVar('x',1,3)
-y = IntVar('y',1,3)
-z = IntVar('z',1,3)
-ux = IntVar('ux',0,1)
-uy = IntVar('uy',0,1)
-uz = IntVar('uz',0,1)
+x = IntVar('x',1,5)
+y = IntVar('y',3,7)
 
 i1  = SearchInstance(
-        [x,y,z,ux,uy,uz],
+        [x,y],
         [
             Constraint(
-                ux == (x == (y*z)) 
-            ),
-            Constraint( 
-                uy == (y == (x*z)) 
-            ),
-            Constraint(
-                (x*y <= z & z <= x+y) 
-                &
-                ((x+1)*(y+1) != z*3) 
+                x == y + 1
             )
         ])
 
@@ -214,5 +220,5 @@ i2 = copy.deepcopy(i1)
 
 i1.propagate()
 
-# -------------------------------------------------
+#====================================================================
 
--- mzn-python/tests.py
+++ mzn-python/tests.py
@@ -1,3 +1,10 @@
 import os
 os.system("clear")
 
+def function(x, y) :
+    return [1,3]
+
+
+[a,b] = function([4,5])
+
+print(f"{a}  {b}")
